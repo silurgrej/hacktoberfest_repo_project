c66bf4e4854857e08e65ad2a532c6b974d92b6cf
Mibadb2920 <116879828+Mibadb2920@users.noreply.github.com>
2022-10-28 15:25:09 +0530

    auth.js file hacktoberfest

---
+++ authorise.js
@@ -0,0 +1,34 @@
+const user = require('../../../../../models/user/user')
+
+const {OAuth2Client} = require('google-auth-library')
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+const client = new OAuth2Client("499316289094-jrcm8c2ugt7d7hobasv2sh2u63a7d1r1.apps.googleusercontent.com")
+
+const googlelogin = (req,res) => {
+  const {tokenId} = req.body
+  client.verifyIdToken({idToken:tokenId,audience : "499316289094-jrcm8c2ugt7d7hobasv2sh2u63a7d1r1.apps.googleusercontent.com"}).then(response=>{
+    const {email_verified,name,email} = response.payload;
+    if(email_verified)
+    {
+        user.findOne({email:email}).exec((err,admin)=>{
+            if(err){
+                return res.status(400).json({error:"Something went wrong"})
+            }
+            else{
+                hacktoberfestif(admin){
+                    const token = jwt.sign({emailID:user.email,type:'user'},process.env.JWT_KEY,{expiresIn:'7d'});
+                    res.json({token,user:{emailID : user.email,type:'user'}})
+                }
+                else{
+                    return res.status(400).json({error:"Something went wrong"})
+                }
+            }
+        })
+    }
+  })
+}
+
+module.exports = { googlelogin}
